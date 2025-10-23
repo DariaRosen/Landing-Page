05e843912da0ebce1492b266378ea95c97e6a085
Daria Rosen <daria.sk135@gmail.com>
2025-10-23 18:19:20 +0300

    Add Services.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Layout } from "./components/Layout";
 import { Hero } from "./components/sections/Hero";
+import { Brands } from "./components/sections/Brands";
 
 function App() {
   return (
     <Layout title="EdgeAI">
       <Hero />
-      {/* <Brands /> */}
+      <Brands />
     </Layout>
   );
 }
