8cee355e89e02b760bd27101d59f8a4dc5ebf0c2
Daria Rosen <daria.sk135@gmail.com>
2025-10-23 18:41:46 +0300

    Add Info and AboutUs .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Layout } from "./components/Layout";
 import { Hero } from "./components/sections/Hero";
 import { Brands } from "./components/sections/Brands";
 import { Services } from "./components/sections/Services";
+import { AboutUs } from "./components/sections/AboutUs";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Brands />
       <Services />
+      <AboutUs />
     </Layout>
   );
 }
